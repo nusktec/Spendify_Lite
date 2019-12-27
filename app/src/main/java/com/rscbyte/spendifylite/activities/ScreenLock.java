c0f9f64ef31f2e6c3823486e08dad7a22b19a4fe
Revelation <nusktecsoft@gmail.com>
2019-12-27 08:21:50 +0100

    SMS Service 80%

---
+++ app/src/main/java/com/rscbyte/spendifylite/activities/ScreenLock.java
@@ -0,0 +1,16 @@
+package com.rscbyte.spendifylite.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rscbyte.spendifylite.R;
+
+public class ScreenLock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_lock);
+    }
+}
